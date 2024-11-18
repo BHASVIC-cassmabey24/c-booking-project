47e7af70277b4c7a0c7477e17363172211909615
BHASVIC-herbieto <herbie.to24@students.bhasvic.ac.uk>
2024-11-18 11:10:27 +0000

    18/11/24

--- check_out.c
+++ check_out.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
 
 
 void check_out() {
     printf("Checking out...");
+    int roomtotal;
+    FILE *filepointer;
+    file = fopen(file_path, "r");
+
 }
