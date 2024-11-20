fa270ac721f984d728f2d52fac9247d74a511374
BHASVIC-herbieto <herbie.to24@students.bhasvic.ac.uk>
2024-11-20 13:32:00 +0000

    20/11/24

--- check_out.c
+++ check_out.c
@@ -4,25 +4,13 @@
 
 
 void check_out() {
-    printf("Checking out...");
-    char first_name[255], surname[255];
-    int day = 0, month = 0, year = 0, total_guests = 0, total_adults = 0, total_children = 0, stay_length = 0, roomnumber = 0, paper = 0, totalprice = 0, randomtempvar;
-    char board[255];
-    char booking_id[255 + 10];
+    char booking_id[255];
+    printf("Checking out...\n");
+    printf("Enter your booking ID:");
+    fflush(stdin);
+    scanf("%c",booking_id);
 
-    FILE *file;
-    //FILE =filepointer
-   // file = fopen(file_path, "r");
-    //fscanf(file, "%s", &first_name);
-    //fscanf(file, "%s", &surname);
-    //fscanf(filepointer, "%d %d %d", &day, &month, &year);
-    //fscanf(filepointer, "%d", &total_guests);
-    //fscanf(filepointer, "%d", &total_adults);
-    //fscanf(filepointer, "%d", &total_children);
-    //fscanf(file, "%s", &board);
-    //fscanf(filepointer, "%d", &stay_length);
-    //fscanf(file, "%s", &booking_id);
-    //fscanf(filepointer, "%d", &roomnumber);
-    //fscanf(filepointer, "%d", &paper);
+
+
 
 }
--- common.c
+++ common.c
@@ -18,11 +18,6 @@
 
 int bookings [4][256];
 
-void generatebooking_id(char surname[], char booking_id[]) {
-    srand(time(NULL));
-    int r = rand();
-    snprintf(booking_id, 255, "%s_%d", surname, r);
-}
 
 int has_numbers(char string[255]) {
     for (int i = 0; i < strlen(string); i++) {
@@ -33,85 +28,6 @@ int has_numbers(char string[255]) {
     return 0;
 }
 
-int validate_board(char board[]) {
-    if (strcmp(board, "FB") == 0) {
-        return 1;
-    } else if (strcmp(board, "HB") == 0) {
-        return 1;
-    } else if (strcmp(board, "BB") == 0) {
-        return 1;
-    }
-    return 0;
-}
-
-
-void create_directory(char *directory) {
-    _mkdir(directory);
-    printf("Directory created successfully: %s\n", directory);
-}
-
-int get_firstname(const char *booking_id) {
-    char first_name[255];
-    char folder[9] = "storage/";
-    FILE *txtfile;
-    strcat(folder, booking_id);
-
-    printf("%s\n", folder);
-    txtfile = fopen(folder, "r");
-
-    if (txtfile == NULL) {
-        printf("Error opening file\n");
-    }
-
-
-
-    printf("Content of the file are: \n");
-}
-
-// reference https://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c
-int check_booking_id(const char *booking_id) {
-    DIR *dir;
-    struct dirent *entry;
-    char path[256];
-    struct stat statbuf;
-
-
-    dir = opendir("storage");
-    if (dir == NULL) {
-        perror("opendir");
-        return 1;
-    }
-
-    // Loop through the directory entries
-    while ((entry = readdir(dir)) != NULL) {
-        // Skip "." and ".." entries
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-            continue;
-        }
-
-        // Construct the full path for each directory entry
-        snprintf(path, sizeof(path), "storage/%s", entry->d_name);
-
-        // Use stat to get information about the file/directory
-        if (stat(path, &statbuf) == 0) {
-            // Check if it's a directory
-            if (S_ISDIR(statbuf.st_mode)) {
-                // Compare the directory name with the booking_id
-                if (strcmp(entry->d_name, booking_id) == 0) {
-                    printf("Match found: Directory with Booking ID '%s' exists.\n", booking_id);
-                    return 1;
-                }
-            }
-        } else {
-            return 0;
-        }
-    }
-
-    // Close the directory
-    closedir(dir);
-
-    return 0;
-}
 // start of booking subprograms:
 bool Incorrect_ID() {
     int check;
