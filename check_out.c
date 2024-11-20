4353f9792d229ad8f1300014dc9e3b2b9144e80a
BHASVIC-herbieto <herbie.to24@students.bhasvic.ac.uk>
2024-11-20 13:53:20 +0000

    20/11/24

--- check_out.c
+++ check_out.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 
 
 void check_out() {
     char booking_id[255];
+    char dob[255];
     printf("Checking out...\n");
     printf("Enter your booking ID:");
     fflush(stdin);
     scanf("%c",booking_id);
+    char* get_booking_field(booking_id,dob);
+
+
 
 
 
