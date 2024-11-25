451bb4960d4d17bf4a00abe2d00c4447f8bf2077
Bhasvic-Harvey-Bell <harvey.bell24@students.bhasvic.ac.uk>
2024-11-25 23:59:57 +0000

    I'm Tired and want to sleep but i finished. (We are still so cooked)

--- book_table.c
+++ book_table.c
@@ -11,98 +11,189 @@ void book_table() {
     while (Start == true) {
         int checkID = 0;
         bool check_Board = false;
-        char BoardType[]="boardtype";
+        bool book =true;
+        //char BoardType[]="boardtype";
         const char ID[256];
-        char *line=get_info(ID,BoardType);
+        int table;
+        int time;
+        char cont;
+        char try;
+        //char *line=get_info(ID,BoardType);
 
-
-
-        printf("\n.");
-        //sleep(1.2);
-        printf("\n.");
-        //sleep(1.2);
-        printf("\n.");
-        //sleep(1.2);
-        printf("\n--------------");
-        printf("\nBooking table:");
-        printf("\n--------------\n");
-
-<<<<<<< HEAD
-
-        printf("\n\n Please enter your given booking ID:");
-        scanf("%s",ID);
-        // way to access booking ID required: going to carry on coding assuming i have it/dont
-        printf("\n \n Checking booking ID...");
-        if (get_info(ID ,"bookingid") == "") {
-            printf("Booking id is invalid");
-        } else {
-            Start = false;
-            printf("Booking id is valid");
-            printf("\n \n Checking board type...");
-            char *line=get_info(ID,"boardtype");
-=======
-        printf("\n \n Checking board type...");
-            sleep(2);
->>>>>>> 1ee9598774691a531f8657e03b8e62281ff4a3c7
-            if (line[0] != 'B') {
-                check_Board = true;
-            }
-
-            if (check_Board == false) {
-                Start=Incorrect_Booking();
-            }
-            else {
-                printf("\n Correct board type...");
-                sleep(2);
-                Start =false;
-            }
-
-<<<<<<< HEAD
-
-=======
-        //booking id check
-        printf("\n\n Please enter your given booking ID:");
-        fflush(stdin);
-        scanf("%s",&ID);
-        // way to access booking ID required: going to carry on coding assuming i have it/dont
-        printf("\n \n Checking booking ID...");
-        sleep(2);
-        checkID =check_booking_id(ID);
-        if (checkID != 1) {
-            Start=Incorrect_ID();
-            if (Start==false) {
-
-            }
-            else {
-                printf("\n Correct Booking ID...");
-            }
-        }
->>>>>>> 1ee9598774691a531f8657e03b8e62281ff4a3c7
         Fill_Table(random);
-        printf("\n \n| Booking Regester |");
-        printf("\n -------------------");
+        while (book =true) {
+            printf("\n \n| Booking Register |");
+            printf("\n-------------------           Here are the three tables with their appropriate times");
 
-        for (int i=0; i<4; i++) {
-            printf("\n");
-            if (i=1) {
-                printf("Naboo:\n");
-            }
-            if (i=2) {
-                printf("Tatooine:\n");
-            }
-            if (i=3){
-                printf("Endor:\n");
-            }
-            for (int j=0; j<2; j++) {
+            sleep(2);
+            printf("\n1.Naboo   7pm:   9pm:           A (1) below means it is booked already while a (0) means available\n");
+            printf("         %d      %d\n",bookings[1][0],bookings[1][1]);
 
-                if (bookings[i][j] == 0) {
-                    printf("Booked");
+            printf("2.Endor   7pm:   9pm:\n");
+            printf("         %d      %d\n",bookings[2][0],bookings[1][1]);
+
+            printf("3.Tatooine 7pm:   9pm:\n");
+            printf("         %d      %d\n",bookings[3][0],bookings[3][1]);
+
+            printf("Please enter which table you would like to book: (1 or 2 or 3)");
+            scanf("%d",&table);
+            fflush(stdin);
+
+            printf("please enter which time you would like 7 or 9");
+            scanf("%d",&time);
+            fflush(stdin);
+
+            if (table==3 || table==2 ||table==2 ||time==9 || time==7) {
+                if (table==1 && time==7) {
+                    if (bookings[1][0]==1) {
+                        printf("\nTable already booked");
+                        sleep(2);
+                    }
+                    else {
+                        bookings[1][0]==1;
+                        printf("Table at Naboo for %d booked!",time);
+                        book =false;
+
+                    }
                 }
-                else if (bookings[i][j] == 1) {
-                    printf("Available");
+                if (table==1 && time==9) {
+                    if (bookings[1][1]==1) {
+                        printf("\nTable already booked");
+                        sleep(2);
+                    }
+                    else {
+                        bookings[1][1]==1;
+                        printf("Table at Naboo for %d booked!",time);
+                        book =false;
 
+                    }
                 }
+                if (table==2 && time==7) {
+                    if (bookings[2][0]==1) {
+                        printf("\nTable already booked");
+                        sleep(2);
+                    }
+                    else {
+                        bookings[2][0]==1;
+                        printf("Table at Endor for %d booked!",time);
+                        book =false;
+
+                    }
+                }
+                if (table==2 && time==9) {
+                    if (bookings[2][1]==1) {
+                        printf("\nTable already booked");
+                        sleep(2);
+                    }
+                    else {
+                        bookings[2][1]==1;
+                        printf("Table at Endor for %d booked!",time);
+                        book =false;
+
+                    }
+                }
+                if (table==3 && time==7) {
+                    if (bookings[3][0]==1) {
+                        printf("\nTable already booked");
+                        sleep(2);
+                    }
+                    else {
+                        bookings[3][0]==1;
+                        printf("Table at tatooine for %d booked!",time);
+                        book =false;
+
+                    }
+                }
+                if (table==3 && time==9) {
+                    if (bookings[3][1]==1) {
+                        printf("\nTable already booked");
+                        sleep(2);
+                    }
+                    else {
+                        bookings[3][1]==1;
+                        printf("Table at Tatooine for %d booked!",time);
+                        book =false;
+
+                    }
+                }
+            }
+            else{
+                printf("\n Incorrect input");
+                printf("\n try again? (y/n)");
+                scanf("%s",&try);
+                if (try == 'n' || try == 'N') {
+                    book =false;
+                    break;
+                }
+
             }
         }
+            printf("\n.");
+            //sleep(1.2);
+            printf("\n.");
+            //sleep(1.2);
+            printf("\n.");
+            //sleep(1.2);
+            printf("\n--------------");
+            printf("\nBooking table:");
+            printf("\n--------------\n");
+
+
+
+            printf("\n\n Please enter your given booking ID:");
+            scanf("%s",ID);
+            // way to access booking ID required: going to carry on coding assuming i have it/dont
+            printf("\n \n Checking booking ID...");
+            if (get_info(ID ,"bookingid") == "") {
+                printf("Booking id is invalid");
+            } else {
+                Start = false;
+                printf("Booking id is valid");
+                printf("\n \n Checking board type...");
+                char *line=get_info(ID,"boardtype");
+
+                printf("\n \n Checking board type...");
+                sleep(2);
+
+                if (line[0] != 'B') {
+                    check_Board = true;
+                }
+
+                if (check_Board == false) {
+                    Start=Incorrect_Booking();
+                }
+                else {
+                    printf("\n Correct board type...");
+                    sleep(2);
+                    Start =false;
+                }
+
+
+
+
+                //booking id check
+                printf("\n\n Please enter your given booking ID:");
+                fflush(stdin);
+                scanf("%s",&ID);
+                // way to access booking ID required: going to carry on coding assuming i have it/dont
+                printf("\n \n Checking booking ID...");
+                sleep(2);
+                checkID =check_booking_id(ID);
+                if (checkID != 1) {
+                    Start=Incorrect_ID();
+                    if (Start==false) {
+
+                    }
+                    else {
+                        printf("\n Correct Booking ID...");
+                    }
+                }
+
+            }
+
+
+
+
     }
 }
+
