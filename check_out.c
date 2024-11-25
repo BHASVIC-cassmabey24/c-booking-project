0566a0f5a24222e09a8a46bbdcdba336862955f8
BHASVIC-herbieto <herbie.to24@students.bhasvic.ac.uk>
2024-11-25 10:47:49 +0000

    20/11/24

--- check_out.c
+++ check_out.c
@@ -4,25 +4,19 @@
 
 
 void check_out() {
-    printf("Checking out...");
-    char first_name[255], surname[255];
-    int day = 0, month = 0, year = 0, total_guests = 0, total_adults = 0, total_children = 0, stay_length = 0, roomnumber = 0, paper = 0, totalprice = 0, randomtempvar;
-    char board[255];
-    char booking_id[255 + 10];
+    printf("Checking out...\n");
+    fflush(stdin);
+    char bookingid[255];
+    printf("Enter you booking ID:");
+    scanf("%s",&bookingid);
+    if (get_info(bookingid, "bookingid") == "") {
+        printf("Invalid booking id");
+    }
 
-    FILE *file;
-    //FILE =filepointer
-   // file = fopen(file_path, "r");
-    //fscanf(file, "%s", &first_name);
-    //fscanf(file, "%s", &surname);
-    //fscanf(filepointer, "%d %d %d", &day, &month, &year);
-    //fscanf(filepointer, "%d", &total_guests);
-    //fscanf(filepointer, "%d", &total_adults);
-    //fscanf(filepointer, "%d", &total_children);
-    //fscanf(file, "%s", &board);
-    //fscanf(filepointer, "%d", &stay_length);
-    //fscanf(file, "%s", &booking_id);
-    //fscanf(filepointer, "%d", &roomnumber);
-    //fscanf(filepointer, "%d", &paper);
+
+    int birthyear = get_info(bookingid,"dob");
+    printf("%p",&birthyear);
+
+
 
 }
